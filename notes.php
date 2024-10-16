05b21e3def29c9741e1f7a841a487d394e2e93c1
kaleycode <readingkale@gmail.com>
2024-10-16 10:01:45 -0700

    Add files via upload

---
+++ notes.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "To print something on the screen:<br><br>";
+echo "Type the word 'echo' and add what you want to output in double quotes. When you are ready to change lines, add a semicolon.";
+?> 
+
+</body>
+</html>
